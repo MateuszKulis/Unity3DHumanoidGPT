091061f8a8de390523e3fe3a248adcffcc70b383
pantrojan <mati22100@op.pl>
2023-11-15 17:56:52 +0100

    create messages and send ChatGPT request

--- Assets/Scripts/ChatGPT/ChatGPTManager.cs
+++ Assets/Scripts/ChatGPT/ChatGPTManager.cs
@@ -1,37 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ChatGPTMessage
+{
+    public string role;
+    public string content;
+}
+
+
+[System.Serializable]
+public class ChatGPTRequest
+{
+    public List<ChatGPTMessage> messages;
+    public int max_tokens;
+    public string model;  
+}
 
 public class ChatGPTManager : MonoBehaviour
 {
-    private string openAIEndpoint = "https://api.openai.com/v1/engines/davinci-codex/completions";  
-    private string openAIKey = "your_key";
+    [SerializeField] private string openAIEndpoint = "https://api.openai.com/v1/chat/completions";
+    [SerializeField] private string openAIKey = "TwójKluczAPI";
 
     private void Awake()
     {
-        GetUserInput("Who  are You?");
+        GetUserInput("Who are You?");
     }
+
     IEnumerator SendChatGPTRequest(string userMessage)
     {
-        string requestData = "{\"prompt\": \"" + userMessage + "\", \"max_tokens\": 100}";
+
+        ChatGPTRequest requestData = new ChatGPTRequest
+        {
+            messages = new List<ChatGPTMessage>
+            {
+                new ChatGPTMessage { role = "system", content = "You are a helpful assistant." },
+                new ChatGPTMessage { role = "user", content = userMessage }
+            },
+            max_tokens = 100,
+            model = "gpt-3.5-turbo"
+        };
+        string jsonData = JsonUtility.ToJson(requestData);
 
         var headers = new Dictionary<string, string>();
         headers.Add("Content-Type", "application/json");
         headers.Add("Authorization", "Bearer " + openAIKey);
 
-        using (UnityWebRequest www = UnityWebRequest.PostWwwForm(openAIEndpoint, requestData))
+        using (UnityWebRequest www = UnityWebRequest.PostWwwForm(openAIEndpoint, "POST"))
         {
             foreach (var header in headers)
             {
                 www.SetRequestHeader(header.Key, header.Value);
             }
 
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
+            www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
             yield return www.SendWebRequest();
 
             if (www.isNetworkError || www.isHttpError)
             {
-                Debug.LogError("Błąd API: " + www.error);
+                Debug.LogError("Błąd API: " + www.error + "\nOdpowiedź API: " + www.downloadHandler.text);
             }
             else
             {
